576f6d932d0738f28982d5004749e8cfc6b363e2
Joel Wolinsky <joelawolinsky@gmail.com>
2020-06-11 13:13:18 +0100

    Change home page to render

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 
+def home(request):
+    return render(request, 'blog/emojiAnimation/anamoji.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
