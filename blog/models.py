e85a92f1dd497449b5b8f4302e0fb4dd937af2f0
Joel Wolinsky <joelawolinsky@gmail.com>
2020-06-28 11:28:32 +0100

    New blog posts

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,11 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     first_para = models.TextField()
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True)
     cover = models.ImageField(upload_to='images/')
+
+    
 
 
     def publish(self):
